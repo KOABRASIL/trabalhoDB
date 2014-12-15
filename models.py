5e51556a5a5a8370088072ccc6a4b5f61a839c68
KOABRASIL <koabrasil@gmail.com>
2014-12-15 11:35:14 -0300

    Update models.py

--- models.py
+++ models.py
@@ -4,3 +4,9 @@ class ZonaEleitoral(models.Model)
 	sigla = models.Chars.Field(max_length=2)
 	nome = models.Chars.Field(max_length=30)
 
+class Candidato(models.Model):
+    NOME = models.CharField(max_length=200)
+    NOME_URNA = models.CharField(max_length=30)
+    ID_NUMERO = models.IntegerField()
+    ID_CARGO = models.CharField(max_length=30)
+    ID = ID_NUMERO
